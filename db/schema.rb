5824bffc85bc2b75673440b8a94582bbb4e30d04
walimike <walimike139@gmail.com>
2020-09-26 12:12:07 +0300

    configure postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_26_082336) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
